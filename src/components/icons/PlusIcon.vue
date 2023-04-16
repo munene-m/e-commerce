b17ba563323a851dbf7fae8f42b5a50f88185311
munene-m <munenenjue18@gmail.com>
2023-04-16 18:36:44 +0300

    installed this for cart

---
+++ src/components/icons/PlusIcon.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24"><path fill="currentColor" d="M11 19v-6H5v-2h6V5h2v6h6v2h-6v6h-2Z"></path></svg>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MaterialSymbolsAdd'
+  }
+  </script>
