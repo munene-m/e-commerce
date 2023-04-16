83668ba6a9b16557fe57083fd2b7b20d816e2d5f
munene-m <munenenjue18@gmail.com>
2023-04-16 18:38:27 +0300

    minor css change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@ const searchItem = ref('')
 
 <template>
 
-  <div class="md:w-4/5 w-11/12 m-auto mt-10 flex items-center flex-col font-open-sans">
+  <div class="md:w-4/5 w-11/12 m-auto mt-24 flex items-center flex-col font-open-sans">
     <img
       class="absolute w-inherit rounded-3xl h-3/4 md:h-2/3 bg-cover"
       src="../assets/booksInShelf.jpg"
@@ -106,11 +106,11 @@ const searchItem = ref('')
   <div class="flex w-5/6 md:w-96 m-auto my-8 md:items-center justify-center md:px-4">
     <input
       v-model="searchItem"
-      class="font-open-sans w-96 h-8 px-6 md:w-inherit lg:w-72 outline-transparent text-gray-800 bg-slate-300 rounded-l-lg"
+      class="font-open-sans w-96 h-8 px-6 md:w-inherit lg:w-72 outline-transparent text-gray-900 bg-slate-300 rounded-l-lg text-sm"
       type="search"
       name="search"
       placeholder="Search books"
-      id=""
+      autocomplete="off"
     />
     <SearchIcon
       class="relative bg-emerald-800 text-white border border-slate-700 right-0 left-0 w-7 rounded-r-lg py-1 h-8"
