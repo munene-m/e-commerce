b1b596bad6d7d706f51ccf0827cbafbdcf40a0f1
munene-m <munenenjue18@gmail.com>
2023-04-16 18:37:23 +0300

    created auth functionality

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,21 +1,32 @@
-import { ref, computed } from "vue";
 import { defineStore, acceptHMRUpdate } from "pinia";
 import axios from "axios"
 
-export const useAuthStore = defineStore('counter', () => {
-    const user = JSON.parse(localStorage.getItem("token"))
+export const useAuthStore = defineStore({
+  id: "auth",
+  state: () => ({
+    user: JSON.parse(localStorage.getItem("token"))
+  }),
+  getters: {},
 
-    async function signup() {
-
+  actions: {
+    async signup(username, email, password) {
+      const response = await axios.post(registerUrl, {
+        username,
+        email,
+        password
+      });
+      this.user = response.data.token;
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+    },
+    async login(email, password) {
+      const response = await axios.post(loginUrl, {
+        email, password
+      })
+      this.user = response.data.token
+      localStorage.setItem("token", JSON.stringify(response.data.token));
     }
-
-    async function login() { 
-
-    }
-
-    return { user, signup, login }
-  })
-  
+  }
+})  
 
 if (import.meta.hot) {
     import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot));
