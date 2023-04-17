f297bd0bdcedaf47244e015106b34bfc034e1b00
munene-m <munenenjue18@gmail.com>
2023-04-17 22:40:03 +0300

    changed height to correctly place footer

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="relative mt-20 font-open-sans">
+    <main class="relative mt-20 font-open-sans h-screen">
         <h1>Categories</h1>
     </main>
     
--- src/views/NewItems.vue
+++ src/views/NewItems.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="relative mt-20 font-open-sans">
+    <main class="relative mt-20 font-open-sans h-screen">
         <h1>Find new items</h1>
     </main>
 </template>
