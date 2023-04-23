db91d4f2c6bd58e4b241f7c3d5b8d9097a61aacd
munene-m <munenenjue18@gmail.com>
2023-04-23 18:52:05 +0300

    added logout and routes that are rendered conditionally

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,8 +4,18 @@ import CartIcon from './icons/CartIcon.vue';
 import PersonIcon from './icons/PersonIcon.vue';
 import { useCartStore } from '../stores/cart';
 import { ref } from 'vue';
+import { useAuthStore } from '../stores/auth';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
 
 const cartStore = useCartStore()
+const authStore = useAuthStore()
+
+const logOut = () => {
+  authStore.logout()
+  router.push('/')
+}
 
   let showMenu = ref(false)
   function toggleNav() {
@@ -14,7 +24,7 @@ const cartStore = useCartStore()
 </script>
 
 <template>
-        <nav class="px-5 py-5 fixed top-0 left-0 right-0 z-10 md:flex md:justify-between md:items-center bg-slate-200 dark:text-gray-200">
+        <nav class="px-5 py-5 fixed top-0 left-0 right-0 z-10 md:flex md:justify-between md:items-center bg-slate-200 bg-opacity-40 firefox:bg-opacity-90 dark:text-gray-200 backdrop-filter backdrop-blur-lg">
         <div class="flex items-center justify-between">
           <RouterLink to="/" class="text-xl flex items-center justify-center gap-1 font-bold text-emerald-800 md:text-2xl font-open-sans">
             E-commerce
@@ -30,7 +40,9 @@ const cartStore = useCartStore()
 
           <li><RouterLink class="text-emerald-800 font-open-sans" to="/categories">Categories</RouterLink></li>
           <li><RouterLink class="text-emerald-800 font-open-sans" to="/new-items">What's new</RouterLink></li>
-          <li><RouterLink class="text-emerald-800 flex items-center font-open-sans" to="/account"><PersonIcon/>Account</RouterLink></li>
+          <li><RouterLink v-if="!authStore.user" class="text-emerald-800 flex items-center font-open-sans" to="/account"><PersonIcon/>Account</RouterLink></li>
+          <li v-if="authStore.user" class="m-0"><button @click="logOut()" class="px-3 py-1 bg-emerald-800 text-white rounded-2xl">Log out</button></li>
+          <li v-if="authStore.admin"><RouterLink class="text-emerald-800 font-open-sans" to="/admin">Admin page</RouterLink></li>
           <li><RouterLink class="text-emerald-800 flex items-center font-open-sans" to="/cart"><CartIcon/><span class="pl-1 bg-emerald-800 text-white rounded-full w-4 h-4 fixed top-4 right-2 text-xs ">{{ cartStore.cart.cartItems.length }}</span></RouterLink></li>
         </ul>
       </nav>
