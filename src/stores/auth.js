1811f4087184e6aee1050ee75b8d7e403d0b89ad
munene-m <munenenjue18@gmail.com>
2023-04-23 18:55:40 +0300

    added logout function

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,26 +4,39 @@ import axios from "axios"
 export const useAuthStore = defineStore({
   id: "auth",
   state: () => ({
-    user: JSON.parse(localStorage.getItem("token"))
+    user: JSON.parse(localStorage.getItem("token")),
+    admin: JSON.parse(localStorage.getItem("adminEmail"))
   }),
   getters: {},
 
   actions: {
     async signup(username, email, password) {
-      const response = await axios.post(registerUrl, {
+      const response = await axios.post('http://localhost:5500/auth/register', {
         username,
         email,
         password
       });
       this.user = response.data.token;
       localStorage.setItem("token", JSON.stringify(response.data.token));
+      if(response.data.email === "admin@email.com"){
+        this.admin = response.data.email
+      }
     },
     async login(email, password) {
-      const response = await axios.post(loginUrl, {
+      const response = await axios.post('http://localhost:5500/auth/login', {
         email, password
       })
       this.user = response.data.token
       localStorage.setItem("token", JSON.stringify(response.data.token));
+      if(response.data.email === "admin@email.com"){
+        this.admin = response.data.email
+        localStorage.setItem("adminEmail", JSON.stringify(response.data.email))
+      }
+    },
+    async logout(){
+      this.user = null
+      localStorage.removeItem("token")
+      localStorage.removeItem("adminEmail")
     }
   }
 })  
