4b5ec115dec6b0d743a0a0794d49d813d115cc69
munene-m <munenenjue18@gmail.com>
2023-05-02 23:25:41 +0300

    installed icon

---
+++ src/components/icons/TagIcon.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48"><mask id="ipTTagOne0"><g fill="none"><path fill="#555" stroke="#fff" strokeLinejoin="round" strokeWidth="4" d="M42.17 29.245L29.262 42.151a3.6 3.6 0 0 1-5.094 0L8 26V8h18l16.17 16.17a3.6 3.6 0 0 1 0 5.075Z"></path><path fill="#fff" fillRule="evenodd" d="M18.5 21a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5Z" clipRule="evenodd"></path></g></mask><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipTTagOne0)"></path></svg>
+  </template>
+  
+  <script>
+  export default {
+    name: 'IconParkTwotoneTagOne'
+  }
+  </script>
