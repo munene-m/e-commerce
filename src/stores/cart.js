f3ca9d5648e1c8bdebea3e2f7f24aa5c57e7150e
munene-m <munenenjue18@gmail.com>
2023-05-04 21:21:00 +0300

    add and remove from cart functionality

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,8 +1,13 @@
-import { reactive } from 'vue'
 import { defineStore, acceptHMRUpdate } from 'pinia'
+import { reactive } from 'vue'
+// import { useAuthStore } from './auth'
+import axios from 'axios'
+
+// const authStore = useAuthStore()
 
 export const useCartStore = defineStore('cart', () => {
-  const STORAGE_KEY = "cartItems"
+  const STORAGE_KEY = 'cartItems'
+  const user = JSON.parse(localStorage.getItem("token"))
 
   const cart = reactive({
     cartItems: []
@@ -10,40 +15,53 @@ export const useCartStore = defineStore('cart', () => {
 
   // Load cart items from local storage on initialization
   if (localStorage.getItem(STORAGE_KEY)) {
-    cart.cartItems = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    cart.cartItems = JSON.parse(localStorage.getItem(STORAGE_KEY))
   }
 
-
-  const addToCart = (item) => {
-      // Check if item already exists in cartItems array
-  const itemInCart = cart.cartItems.find(cartItem => cartItem.id === item.id);
-  if (itemInCart) {
-    // If item already exists, increment quantity
-    itemInCart.quantity++;
-  } else {
-    // Otherwise, add item to cartItems array
-    cart.cartItems.push({ ...item, quantity: 1 });
+  const addToCart = async (productId, name, image, price, quantity) => {
+    // Check if item already exists in cartItems array
+    await axios.post("http://localhost:5500/cart/add", { productId, name, image, price, quantity },
+        { headers: { Authorization: `Bearer ${user}` }}
+      ).then((response) => {
+        console.log(response.data)
+        const item = response.data
+        const itemInCart = cart.cartItems.find(cartItem => cartItem._id === response.data._id);
+            if (itemInCart) {
+      // If item already exists, increment quantity
+               itemInCart.quantity++;
+             } else {
+      // Otherwise, add item to cartItems array
+             cart.cartItems.push({ ...item, quantity: 1 });
+    }
+    console.log(cart.cartItems);
+      })
+      .catch((err) => console.log(err))
   }
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems));
-  }
 
-  const removeFromCart = (id) => {
-    cart.cartItems = cart.cartItems.filter(item => item.id !== id)
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems));
+  const removeFromCart = async (id) => {
+    await axios.delete(`http://localhost:5500/cart/delete/${id}`,
+    { headers: { Authorization: `Bearer ${user}` }})
+    .then(response => {
+      console.log(response.data)
+      cart.cartItems = cart.cartItems.filter((item) => item._id !== id)
+    })
+    .catch(err => console.log(err))
+    // cart.cartItems = cart.cartItems.filter((item) => item.id !== id)
+    // localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems))
   }
 
   const incrementQuantity = (id) => {
-    const item = cart.cartItems.find(item => item.id === id)
+    const item = cart.cartItems.find((item) => item._id === id)
     if (item) {
-      item.quantity++;
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems));
+      item.quantity++
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems))
     }
   }
   const decrementQuantity = (id) => {
-    const item = cart.cartItems.find(item => item.id === id)
+    const item = cart.cartItems.find((item) => item._id === id)
     if (item.quantity > 1) {
-      item.quantity--;
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems));
+      item.quantity--
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(cart.cartItems))
     }
   }
 
@@ -51,5 +69,5 @@ export const useCartStore = defineStore('cart', () => {
 })
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot));
+  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot))
 }
