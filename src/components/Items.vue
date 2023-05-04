6a7a91dcbdddbf3d9d44abf768162cc190457283
munene-m <munenenjue18@gmail.com>
2023-05-04 21:20:16 +0300

    created addtocart function and getProducts for the home page

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, reactive, onMounted } from 'vue'
 import { useCartStore } from '../stores/cart'
 import { useAuthStore } from '../stores/auth'
 import axios from 'axios';
@@ -7,130 +7,78 @@ import axios from 'axios';
 const cartStore = useCartStore()
 const authStore = useAuthStore()
 
-function addToCart(item) {
-  cartStore.addToCart(item)
-  console.log(cartStore.cart.cartItems)
+function addToCart(id, name, quantity, image, price) {
+
+  cartStore.addToCart(id,name,image, price,quantity )
+
+  // await axios.get(`http://localhost/products/product/${id}`)
+  // .then(response => {
+  //   console.log(response.data)
+  //   productData.productId = response.data._id
+  //   productData.name = response.data.name
+  //   productData.price = response.data.price
+  //   productData.image = response.data.image
+  //   productData.quantity = response.data.quantity
+
+  // })
+  // .catch(err => console.log(err))
 }
 
-const books = ref([
-  {
-    id: 1,
-    name: 'The Great Gatsby',
-    description: 'A novel by F. Scott Fitzgerald',
-    price: 10.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 2,
-    name: 'To Kill a Mockingbird',
-    description: 'A novel by Harper Lee',
-    price: 12.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 3,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 4,
-    name: 'The Great Gatsby',
-    description: 'A novel by F. Scott Fitzgerald',
-    price: 10.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 5,
-    name: 'To Kill a Mockingbird',
-    description: 'A novel by Harper Lee',
-    price: 12.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 6,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 7,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 8,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 9,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 10,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  },
-  {
-    id: 11,
-    name: '1984',
-    description: 'A novel by George Orwell',
-    price: 9.99,
-    image: 'https://via.placeholder.com/150',
-    quantity: 0
-  }
-])
+const items = ref([])
+
+async function getProducts() {
+  await axios.get("http://localhost:5500/products", {
+    headers: {
+      Authorization: `Bearer ${authStore.user}`
+    }
+  })
+  .then(response => {
+    items.value.push(response.data)
+    console.log(response.data)
+  }).catch(err => console.log(err))
+}
+onMounted(() => {
+  getProducts()
+})
+
 const props = defineProps({
   searchItem: String
 })
-const filteredBooks = computed(() => {
-  return books.value.filter((book) => {
-    return book.name.toLowerCase().includes(props.searchItem.toLowerCase())
-  })
+const filteredProducts = computed(() => {
+  let result = []
+  for (let i = 0; i < items.value.length; i++) {
+    let innerArray = items.value[i]
+    for (let j = 0; j < innerArray.length; j++) {
+      let item = innerArray[j]
+      if (item.name.toLowerCase().includes(props.searchItem.toLowerCase())) {
+        result.push(item)
+      }
+    }
+  }
+  return result
 })
-const booksFound = computed(() => {
-  return filteredBooks.value.length > 0
+const productsFound = computed(() => {
+  return filteredProducts.value.length > 0
 })
 async function removeItem (id) {
-  await axios.delete(`http://localhost:5500/products/delete/${id}`, {
-      headers: { Authorization: `Bearer ${authStore.user}` }
-    })
-  .then(response => console.log(response.data))
-  .catch(err => console.log(err))
+  cartStore.removeFromCart(id)
+  // await axios.delete(`http://localhost:5500/products/delete/${id}`, {
+  //     headers: { Authorization: `Bearer ${authStore.user}` }
+  //   })
+  // .then(response => console.log(response.data))
+  // .catch(err => console.log(err))
 }
 </script>
 
 <template>
-  <div v-if="booksFound" class="flex overflow-auto pb-4 whitespace-nowrap items-end sm:grid sm:gap-5 sm:grid-cols-2 md:grid-cols-4 sm:place-items-center font-open-sans"
+  <div v-if="productsFound" class="flex overflow-auto pb-4 whitespace-nowrap items-end sm:grid sm:gap-5 sm:grid-cols-2 md:grid-cols-4 sm:place-items-center font-open-sans"
     style="::-webkit-scrollbar { display: none; }">
-    <div v-for="book in filteredBooks" :key="book.id" class="p-4 rounded-lg sm:w-5/6 w-full h-full flex flex-col items-start mr-6">
-      <img class="w-full h-full rounded object-cover" :src="book.image" :alt="book.name" />
-      <h2 class="mt-2 font-bold text-emerald-800">{{ book.name }}</h2>
-      <p class="text-sm">{{ book.description }}</p>
-      <p class="text-xs sm:text-sm">Price: {{ book.price }}</p>
-      <button v-if="!authStore.admin" @click="addToCart(book)"
+    <div v-for="product in filteredProducts" :key="product._id" class="p-4 rounded-lg sm:w-5/6 w-full h-full flex flex-col items-start mr-6">
+      <img class="w-full h-full rounded object-cover" :src="product.image" :alt="product.name" />
+      <h2 class="mt-2 font-bold text-emerald-800">{{ product.name }}</h2>
+      <p class="text-sm">{{ product.description }}</p>
+      <p class="text-xs sm:text-sm">Price: {{ product.price }}</p>
+      <button v-if="!authStore.admin" @click="addToCart(product._id, product.name, product.quantity, product.image, product.price)"
         class="bg-emerald-800 text-white rounded-3xl border-2 border-emerald-800 px-3 py-1 mt-2 text-xs cursor-pointer hover:scale-95 transition duration-500">
         Add to cart
       </button>
@@ -138,13 +86,13 @@ async function removeItem (id) {
         <button class="bg-emerald-800 text-white rounded-3xl border-2 border-emerald-800 px-3 py-1 mt-2 text-xs cursor-pointer hover:scale-95 transition duration-500">
           Edit
         </button>
-        <button @click="removeItem(book.id)" class="bg-red-500 text-white rounded-3xl border-2 border-red-500 px-3 py-1 mt-2 text-xs cursor-pointer hover:scale-95 transition duration-500 mx-1">
+        <button @click="removeItem(product._id)" class="bg-red-500 text-white rounded-3xl border-2 border-red-500 px-3 py-1 mt-2 text-xs cursor-pointer hover:scale-95 transition duration-500 mx-1">
           Remove
         </button>
       </div>
     </div>
   </div>
   <div v-else>
-    <p class="text-emerald-800 text-center">The book you have searched is unavailable.</p>
+    <p class="text-emerald-800 text-center">The item you have searched is unavailable.</p>
   </div>
 </template>
