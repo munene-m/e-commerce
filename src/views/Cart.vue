ffaba103f707b69de03377c725c383809fe76abb
munene-m <munenenjue18@gmail.com>
2023-05-04 21:21:36 +0300

    function to get all cartItems

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,35 +1,54 @@
 <script setup>
-import { onMounted, computed } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useCartStore } from '../stores/cart'
+import { useAuthStore } from '../stores/auth'
 import DeleteIcon from '../components/icons/DeleteIcon.vue'
 import PlusIcon from '../components/icons/PlusIcon.vue'
 import MinusIcon from '../components/icons/MinusIcon.vue'
 import ArrowIcon from '../components/icons/ArrowIcon.vue'
+import axios from 'axios'
 
 const cartStore = useCartStore()
+const authStore = useAuthStore()
 const router = useRouter()
 
-let cartItems = computed(() => cartStore.cart.cartItems)
+// let cartItems = computed(() => cartStore.cart.cartItems)
+const cartItems = ref([])
+
+async function getCartItems() {
+  await axios.get("http://localhost:5500/cart", {
+    headers: {
+      Authorization: `Bearer ${authStore.user}`
+    }
+  }).then(response => {
+    cartItems.value.push(response.data)
+  }).catch(err => console.log(err))
+}
+
+onMounted(() => {
+  getCartItems()
+})
 
 function removeItem(id) {
   cartStore.removeFromCart(id)
 }
 
 function increase(id) {
-  const item = cartStore.cart.cartItems.find((item) => item.id === id)
+  const item = cartStore.cart.cartItems.find((item) => item._id === id)
   if (item) {
     cartStore.incrementQuantity(id)
   }
 }
 function decrease(id) {
-  const item = cartStore.cart.cartItems.find((item) => item.id === id)
+  const item = cartStore.cart.cartItems.find((item) => item._id === id)
   if (item) {
     cartStore.decrementQuantity(id)
   }
 }
 onMounted(() => {
   console.log(cartItems.value)
+  cartItems
 })
 
 const isCart = computed(() => {
@@ -42,28 +61,29 @@ const redirectToCheckout = () => {
 
 <template>
   <main v-if="isCart" class="font-open-sans relative mt-28 mb-20">
-    <div class="bg-slate-200 rounded p-2 m-auto mb-3 w-4/5 sm:w-2/3 md:w-1/2 grid grid-cols-1 sm:grid-cols-2 place-items-evenly"
-      v-for="item in cartItems"
-      :key="item.id">
+<div v-for="(cartItem,index) in cartItems" :index="index">
+  <div class="bg-slate-200 rounded p-2 m-auto mb-3 w-4/5 sm:w-2/3 md:w-1/2 grid grid-cols-1 sm:grid-cols-2 place-items-evenly"
+      v-for="item in cartItem" :key="item._id">
       <img class="w-full sm:w-full md:w-40" :src="item.image" alt="" />
       <div class="flex flex-col items-start justify-between  sm:items-end sm:justify-between">
         <h1 class="text-emerald-800 font-bold text-lg text-center">{{ item.name }}</h1>
-        <p class="font-bold sm:text-sm">{{ item.description }}</p>
+        <!-- <p class="font-bold sm:text-sm">{{ item.description }}</p> -->
         <p>Price: {{ item.price }}</p>
         <p class="flex items-center">
           Quantity:
-          <MinusIcon @click="decrease(item.id)" class="cursor-pointer mx-1 bg-slate-400 rounded-full" />
+          <MinusIcon @click="decrease(item._id)" class="cursor-pointer mx-1 bg-slate-400 rounded-full" />
           {{ item.quantity }}
-          <PlusIcon @click="increase(item.id)" class="cursor-pointer mx-1 bg-slate-400 rounded-full" />
+          <PlusIcon @click="increase(item._id)" class="cursor-pointer mx-1 bg-slate-400 rounded-full" />
         </p>
         <button
-          @click="removeItem(item.id)"
+          @click="removeItem(item._id)"
           class="bg-red-600 text-white rounded-lg border-2 border-red-600 px-3 py-1 mt-2 text-xs cursor-pointer hover:scale-95 transition duration-500 flex items-center"
         >
           Remove <DeleteIcon />
         </button>
       </div>
     </div>
+</div>
     <button @click="redirectToCheckout()" class="bg-emerald-800 m-auto text-white rounded-lg border-2 border-emerald-800  px-3 py-2 mt-9 text-sm cursor-pointer hover:scale-95 transition duration-500 flex items-center justify-center">Proceed to checkout <ArrowIcon/></button>
 
   </main>
