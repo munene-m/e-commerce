404b650544d9a10b09e569624d9564d8bb31768b
munene-m <munenenjue18@gmail.com>
2023-05-06 18:59:29 +0300

    reusable modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  show: Boolean,
+});
+</script>
+
+<template>
+  <div v-if="show" class="fixed inset-0 pt-20 bg-opacity-10 backdrop-filter backdrop-blur-sm grid place-items-center">
+    <div class="bg-white px-2 py-1 max-w-4xl border rounded-xl">
+        <slot name="form"></slot>
+
+      <!-- <footer>
+        <button >Submit</button>
+      </footer> -->
+    </div>
+  </div>
+</template>
+
