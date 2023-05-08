f9f2568eafce5b73e2f4febbb4941b7ddd837277
munene-m <munenenjue18@gmail.com>
2023-05-08 16:02:31 +0300

    saving cart items to localstorage and retrieving them to the cart page

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -16,6 +16,9 @@ const router = useRouter()
 // let cartItems = computed(() => cartStore.cart.cartItems)
 const cartItems = ref([])
 
+const cartItem = JSON.parse(localStorage.getItem("cart"))
+
+
 async function getCartItems() {
   await axios.get(`https://m-duka.onrender.com/cart/${authStore.username}`, {
     headers: {
@@ -69,10 +72,9 @@ onMounted(() => {
 
 <template>
   <main v-if="isCart" class="font-open-sans relative mt-28 mb-20">
-<div v-for="(cartItem,index) in cartItems" :index="index">
-  <div class="bg-slate-200 rounded p-2 m-auto mb-3 w-4/5 sm:w-2/3 md:w-1/2 grid grid-cols-1 sm:grid-cols-2 place-items-evenly"
-      v-for="item in cartItem" :key="item._id">
-      <img class="w-full sm:w-full md:w-40" :src="item.image" alt="" />
+
+  <div v-for="item in cartItem" :key="item._id">
+    <img class="w-full sm:w-full md:w-40" :src="item.image" alt="" />
       <div class="flex flex-col items-start justify-between  sm:items-end sm:justify-between">
         <h1 class="text-emerald-800 font-bold text-lg text-center">{{ item.name }}</h1>
         <!-- <p class="font-bold sm:text-sm">{{ item.description }}</p> -->
@@ -87,14 +89,39 @@ onMounted(() => {
           Remove <DeleteIcon />
         </button>
       </div>
-    </div>
-</div>
-    <button v-if="!isCart" @click="redirectToCheckout(item._id)" class="bg-emerald-800 m-auto text-white rounded-lg border-2 border-emerald-800  px-3 py-2 mt-9 text-sm cursor-pointer hover:scale-95 transition duration-500 flex items-center justify-center">Proceed to checkout <ArrowIcon/></button>
-
+  </div>
+  <button v-if="!isCart" @click="redirectToCheckout(item._id)" class="bg-emerald-800 m-auto text-white rounded-lg border-2 border-emerald-800  px-3 py-2 mt-9 text-sm cursor-pointer hover:scale-95 transition duration-500 flex items-center justify-center">Proceed to checkout <ArrowIcon/></button>
   </main>
   <div v-else>
     <p class="font-open-sans text-emerald-800 text-center mt-24 h-screen">Cart is empty</p>
   </div>
+  <!-- <main v-if="isCart" class="font-open-sans relative mt-28 mb-20">
+    <div v-for="(cartItem,index) in cartItems" :index="index">
+      <div class="bg-slate-200 rounded p-2 m-auto mb-3 w-4/5 sm:w-2/3 md:w-1/2 grid grid-cols-1 sm:grid-cols-2 place-items-evenly"
+      v-for="item in cartItem" :key="item._id">
+      <img class="w-full sm:w-full md:w-40" :src="item.image" alt="" />
+      <div class="flex flex-col items-start justify-between  sm:items-end sm:justify-between">
+        <h1 class="text-emerald-800 font-bold text-lg text-center">{{ item.name }}</h1>
+        <p class="font-bold sm:text-sm">{{ item.description }}</p>
+        <p>Price: {{ item.price }}</p>
+        <p class="flex items-center">
+          Quantity:
+          <MinusIcon @click="decrease(item._id, item.quantity)" class="cursor-pointer mx-1 bg-slate-400 rounded-full" />
+          {{ item.quantity }}
+          <PlusIcon @click="increase(item._id, item.quantity)" class="cursor-pointer mx-1 bg-slate-400 rounded-full" />
+        </p>
+        <button @click="removeItem(item._id)" class="bg-red-600 text-white rounded-lg border-2 border-red-600 px-3 py-1 mt-2 text-xs cursor-pointer hover:scale-95 transition duration-500 flex items-center">
+          Remove <DeleteIcon />
+        </button>
+      </div>
+    </div>
+</div>
+  <button v-if="!isCart" @click="redirectToCheckout(item._id)" class="bg-emerald-800 m-auto text-white rounded-lg border-2 border-emerald-800  px-3 py-2 mt-9 text-sm cursor-pointer hover:scale-95 transition duration-500 flex items-center justify-center">Proceed to checkout <ArrowIcon/></button>
+
+  </main>
+  <div v-else>
+    <p class="font-open-sans text-emerald-800 text-center mt-24 h-screen">Cart is empty</p>
+  </div> -->
 </template>
 
 <style></style>
