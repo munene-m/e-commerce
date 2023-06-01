cb1560e7c5db90be5dc02e9104e44f49bbc4a04e
munene-m <munenenjue18@gmail.com>
2023-06-01 14:32:01 +0300

    chore: wrapped the signup function in trycatch block and handled response to save more details to localStorage

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -12,16 +12,23 @@ export const useAuthStore = defineStore({
 
   actions: {
     async signup(username, email, password) {
-      const response = await axios.post('https://m-duka.onrender.com/auth/register', {
-        username,
-        email,
-        password
-      })
-        this.user = response.data.token;
-        localStorage.setItem("token", JSON.stringify(response.data.token));
-        if(response.data.email === "admin@email.com"){
-          this.admin = response.data.email
-        }
+      try {
+        const response = await axios.post('https://m-duka.onrender.com/auth/register', {
+          username,
+          email,
+          password
+        })
+        console.log(response.data)
+          this.user = response.data.token;
+          localStorage.setItem("token", JSON.stringify(response.data.token));
+      localStorage.setItem("username", JSON.stringify(response.data.username))
+
+          if(response.data.email === "admin@email.com"){
+            this.admin = response.data.email
+          }
+      } catch (err) {
+        console.log(err)
+      }
     },
     async login(email, password) {
       const response = await axios.post('https://m-duka.onrender.com/auth/login', {
