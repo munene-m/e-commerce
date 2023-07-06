c949f59e9535ef86818622502d4b1c7b937922e6
munene-m <munenenjue18@gmail.com>
2023-07-06 13:24:05 +0300

    removed login and signup functions from store

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -11,42 +11,12 @@ export const useAuthStore = defineStore({
   getters: {},
 
   actions: {
-    async signup(username, email, password) {
-      try {
-        const response = await axios.post('https://m-duka.onrender.com/auth/register', {
-          username,
-          email,
-          password
-        })
-        // console.log(response.data)
-          this.user = response.data.token;
-          localStorage.setItem("token", JSON.stringify(response.data.token));
-      localStorage.setItem("username", JSON.stringify(response.data.username))
-
-          if(response.data.email === "admin@email.com"){
-            this.admin = response.data.email
-          }
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    async login(email, password) {
-      const response = await axios.post('https://m-duka.onrender.com/auth/login', {
-        email, password
-      })
-      this.user = response.data.token
-      localStorage.setItem("token", JSON.stringify(response.data.token));
-      localStorage.setItem("username", JSON.stringify(response.data.username))
-      if(response.data.email === "admin@email.com"){
-        this.admin = response.data.email
-        localStorage.setItem("adminEmail", JSON.stringify(response.data.email))
-      }
-    },
     async logout(){
       this.user = null
       localStorage.removeItem("token")
       localStorage.removeItem("adminEmail")
       localStorage.removeItem("username")
+      location.reload()
     }
   }
 })  
